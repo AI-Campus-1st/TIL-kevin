841830c2d50a162ff27a6091f7540cfc0ff36a05
kevin110419 <kevin9448@gmail.com>
2026-07-31 23:44:14 +0900

    260730

---
+++ workspace/260730/app.py
@@ -0,0 +1,10 @@
+def main():
+    print('Hello world')
+    num1=int(input('숫자를 입력해 주세요:'))
+    num2=int(input('두 번째 숫자를 입력해 주세요:'))
+    total=num1+num2
+    print('합계: %d'%total)
+    return
+
+if __name__=='__main__':
+    main()
