76a675d9f41233fe5b5f31949c1a989720162eda
kevin110419 <kevin9448@gmail.com>
2026-09-12 00:43:09 +0900

    260911.1

--- workspace/260903/assignment/260903_AI데이터분석.py
+++ workspace/260903/assignment/260903_AI데이터분석.py
@@ -4,9 +4,30 @@ import pandas as pd
 from streamlit_autorefresh import st_autorefresh
 import plotly.graph_objects as go
 from plotly.subplots import make_subplots
+import os
+from dotenv import load_dotenv
 
 engine = create_engine('sqlite:///stocks.db')
 
+load_dotenv()
+# .env 저장
+host = os.getenv('DB_HOST', '')
+port = int(os.getenv('DB_PORT', 3306))
+user = os.getenv('DB_USER', '')
+password = os.getenv('DB_PASSWORD', '')
+database = os.getenv('DB_NAME', 'stock_db')
+
+engine = create_engine(f'mysql+pymysql://{user}:{password}@{host}:{port}/{database}')
+
+def load_stock_infos():
+    with engine.connect() as conn:
+        query = "SELECT * FROM tb_stock"
+        return pd.read_sql(query, conn)
+
+def load_data(stock_id):
+    with engine.connect() as conn:
+        query = "SELECT * FROM tb_price WHERE stock_id=%s ORDER BY created_at DESC LIMIT 100"
+        return pd.read_sql(query, conn, params=(stock_id, ))
 
 def load_data():
     with engine.connect() as conn:
